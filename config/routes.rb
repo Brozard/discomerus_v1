918a04666ecdc4c0f20b989ec1a12efe89edfe59
Brian Nickerson <nickerson.brian@gmail.com>
2016-05-23 18:43:51 -0400

    Update Stylesheet and some forms, Manufacturers Controller and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  get 'static/signup'
-
-  get 'static/signin'
-
   get 'static/about'
 
 
