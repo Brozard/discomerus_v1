0531fde920370b194b95634e06764b39cc9f16d9
Brian Nickerson <nickerson.brian@gmail.com>
2016-05-28 19:04:56 -0400

    Remove 'picture' from Product table via migration

--- db/migrate/20160524223512_add_attachment_photo_to_pictures.rb
+++ db/migrate/20160524223512_add_attachment_photo_to_pictures.rb
@@ -11,12 +11,3 @@ class AddAttachmentPhotoToPictures < ActiveRecord::Migration
     remove_reference :pictures, :product
   end
 end
-
-# def self.up
-#    add_attachment :products, :photo
-#  end
-#
-#  def self.down
-#    remove_attachment :products, :photo
-#  end
-# end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528201528) do
+ActiveRecord::Schema.define(version: 20160528230402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,7 +89,6 @@ ActiveRecord::Schema.define(version: 20160528201528) do
     t.integer  "manufacturer_id"
     t.integer  "buyer_id"
     t.integer  "category_id"
-    t.string   "picture"
   end
 
   add_index "products", ["buyer_id"], name: "index_products_on_buyer_id", using: :btree
