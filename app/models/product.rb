14c080d4ed0318fbe96b70f58be44a36ed3a7874
Brian Nickerson <nickerson.brian@gmail.com>
2016-05-27 21:44:23 -0400

    Create global application scope, improve Category and Price search/input, update models - Date search/input still not working

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,19 +4,17 @@ class Product < ActiveRecord::Base
   belongs_to :category
   has_many :pictures
 
+  def show_category
+    category_id == nil ? "" : category.category_name
+  end
+
   default_scope { order('id ASC')}
 
-  def num_check(num)
-    if num.first.nil?
-      nil
-    else
-      num.first
-    end
-  end
+  scope :order_by_product_name, -> { unscoped.order("name ASC") }
 
   scope :user_products, -> (user) { where buyer_id: user }
 
   scope :by_name, -> (name) { where("name LIKE ?", "%#{name}%") }
   scope :by_price, -> (min_price, max_price) { where("price >= ? AND price <= ?", (min_price.to_d * 100).to_i, (max_price.to_d * 100).to_i) }
-  # scope :by_category, -> (category) { joins(:category).where("categories.category LIKE ?", "%#{category}%").references(:categories) }
+  scope :by_category, -> (category) { where("category_id = ?", category.to_i) }
 end
